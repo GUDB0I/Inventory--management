a4988eadbf676e1652b9e915e2a91dd313627031
GUDB0I <nguyenminhvu9a9@gmail.com>
2026-08-07 10:51:37 +0700

    Configure CORS

---
+++ inventory-management/config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://inventory-management-y4bo-git-master-vu15.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
